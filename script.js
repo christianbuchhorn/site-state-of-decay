8e33b35babffc834f75753f282cdc6dfc080a000
Carlos <carlos-christia@outlook.com>
2023-12-31 17:13:43 -0400

    arrumei o video

---
+++ script.js
@@ -0,0 +1,19 @@
+function closevideo() {
+
+    let tagclose = document.getElementById ('switch')
+
+    tagclose.classList.add ('esconder')
+
+
+       
+}
+
+function openvideo() {
+
+    let tagclose = document.getElementById ('switch')
+
+    tagclose.classList.remove ('esconder')
+
+
+       
+}
